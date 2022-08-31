179bc8c338c21df4b3ce9eae154108d002e3e4ff
Rayssa Correia <rayssamicorreia@gmail.com>
2022-08-31 17:42:14 -0300

    Aula 3

--- aula2/Catalago/app/src/main/java/com/example/catalogodigital/MainActivity.kt
+++ aula2/Catalago/app/src/main/java/com/example/catalogodigital/MainActivity.kt
@@ -5,16 +5,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btn = findViewById<Button>(R.id.button)
-
-        btn.setOnClickListener(){
-            startActivity(Intent(this,Fde_600w_Page::class.java))
+        val mainToolBar = findViewById<MaterialToolbar>(R.id.mainToolBar)
+        mainToolBar.setNavigationOnClickListener{
+            Toast.makeText(this, "Hamburger Pressionado!!!", Toast.LENGTH_SHORT).show()
         }
+
+        mainToolBar.setOnMenuItemClickListener{ menuItem ->
+            when (menuItem.itemId){
+                R.id.menu_item_notifications -> {
+                    Toast.makeText(this,
+                        "Ir para a página de notificaões !!!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    true
+
+                }
+                R.id.menu_item_cart -> {
+                    Toast.makeText(
+                        this,
+                        "Ir para a página do carrinho!!!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
+
     }
 }
