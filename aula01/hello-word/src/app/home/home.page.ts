b9e5f7929ab16e1e139e29cdcee6669fccce530c
Rayssa Correia <rayssamicorreia@gmail.com>
2022-09-30 17:18:39 -0300

    atualizacao

---
+++ aula01/hello-word/src/app/home/home.page.ts
@@ -0,0 +1,17 @@
+//! FAZER AS FUNÇÕES PARA ESSA PAGINA
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private router: Router) {}
+
+  goToPage() {
+    this.router.navigate(['/cidades']);
+  }
+}
